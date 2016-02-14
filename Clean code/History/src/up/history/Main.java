11a31a59f6ee6c88de2c9beaefc6c7bc06415714
Sergey <sergeishmaro@gmail.com>
2016-02-14 17:24:19 +0300

    Add Clean code

---
+++ Clean code/History/src/up/history/Main.java
@@ -0,0 +1,11 @@
+package up.history;
+import javax.json.*;
+import com.google.gson.*;
+import com.fasterxml.jackson.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
